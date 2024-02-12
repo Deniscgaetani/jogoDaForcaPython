d53f662f326f37c7285270160d4584c723d76971
Gaetani <deniscgaetani@gmail.com>
2024-02-12 19:29:50 -0300

    Forca Versão 2

--- jogo.py
+++ jogo.py
@@ -13,6 +13,82 @@ def limpa_tela():
     else:
         _ = system('clear')
 
+# Função que desenha a forca na tela
+def display_hangman(chances):
+
+    # Lista de estágios da forca
+    stages = [  # estágio 6 (final)
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / \\
+                   -
+                """,
+                # estágio 5
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / 
+                   -
+                """,
+                # estágio 4
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |      
+                   -
+                """,
+                # estágio 3
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|
+                   |      |
+                   |     
+                   -
+                """,
+                # estágio 2
+                """
+                   --------
+                   |      |
+                   |      O
+                   |      |
+                   |      |
+                   |     
+                   -
+                """,
+                # estágio 1
+                """
+                   --------
+                   |      |
+                   |      O
+                   |    
+                   |      
+                   |     
+                   -
+                """,
+                # estágio 0
+                """
+                   --------
+                   |      |
+                   |      
+                   |    
+                   |      
+                   |     
+                   -
+                """
+    ]
+    return stages[chances]
 
 # Função
 def game():
@@ -27,46 +103,60 @@ def game():
     # Escolhe randomicamente uma palavra
     palavra = random.choice(palavras)
 
-    # List comprehension
-    letras_descobertas = ['_' for letra in palavra]
+    # Lista  de letras  da palavra
+    lista_letras_palavras = [letra for letra in palavra]
+
+    # Cria o tabuleiro com o caracter "_" multiplicado pelo comprimento da palavra
+    tabuleiro = ["_"] * len(palavra)
 
     # Número de chances
     chances = 6
 
-    # Lista para as letras erradas
-    letras_erradas = []
+    # Lista para as letras digitadas
+    letras_tentativas = []
 
     # Loop enquanto número de chances for maior do que zero
     while chances > 0:
 
-        # Print
-        print(" ".join(letras_descobertas))
-        print("\nChances restantes:", chances)
-        print("Letras erradas:", " ".join(letras_erradas))
+        print(display_hangman(chances))
+        print("Palavra: ", tabuleiro)
+        print("\n")
 
         # Tentativa
-        tentativa = input("\nDigite uma letra: ").lower()
+        tentativa = input("\nDigite uma letra: ")
 
         # Condicional
-        if tentativa in palavra:
-            index = 0
+        if tentativa in letras_tentativas:
+            print("Você já tentou essa letra. Escolha outra!")
+            continue
 
-            for letra in palavra:
-                if tentativa == letra:
-                    letras_descobertas[index] = letra
-                index += 1
+        # Lista de tentativas (letras)
+        letras_tentativas.append(tentativa)
+
+        # Condicional
+        if tentativa in lista_letras_palavras:
+
+            print("Você acertou a letra!")
+
+            # Loop
+            for indice in range(len(lista_letras_palavras)):
+
+                # Condicional
+                if lista_letras_palavras[indice] == tentativa:
+                    tabuleiro[indice] = tentativa
+
+            # Se todos os espaços foram preenchidos, o jogo acabou
+            if "_" not in tabuleiro:
+                print("\nVocê venceu! A palavra era: {}".format(palavra))
+                break
         else:
+            print("Ops. Essa letra não está na palavra!")
+            # Decremento
             chances -= 1
-            letras_erradas.append(tentativa)
-
-        # Condicional
-        if "_" not in letras_descobertas:
-            print("\nVocê venceu, a palavra era:", palavra)
-            break
 
     # Condicional
-    if "_" in letras_descobertas:
-        print("\nVocê perdeu, a palavra era:", palavra)
+    if "_" in tabuleiro:
+        print("\nVocê perdeu! A palavra era: {}.".format(palavra))
 
 
 # Bloco main
